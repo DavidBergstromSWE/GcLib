1ca6e80ea8b84f09a3faf6625cd388dc444571af
David Bergström <david.bergstrom@mysimlabs.com>
2024-10-10 18:32:20 +0200

    Added some more xml comments to sample console app.

--- samples/GcLib.Samples.ConsoleApp/Program.cs
+++ samples/GcLib.Samples.ConsoleApp/Program.cs
@@ -1,25 +1,25 @@
 ﻿using GcLib;
 
-/* The simple example shows how to initialize the library, discover available devices and acquire images from a virtual camera simulator. The app should produce two images in the output directory. */
+/* The simple example shows how to initialize the library, discover available devices, change device parameters and acquire images from it. The app should produce two images in the output directory. */
 
-// Library always needs to be initialized before it can be used. Initialization will discover which APIs are supported on the current system.
+// Initialization of the library will discover which APIs are supported on the current system (always needed before the library can be used).
 GcLibrary.Init();
 
-// Instantiate system.
+// Instantiate system level, from which devices can be opened (note: only one system level can be used at a time!).
 using var system = new GcSystem();
 
 // Ask system to enumerate available devices.
 system.UpdateDeviceList();
 var cameraList = system.GetDeviceList();
 
-// Pick the first available one (will be an instance of VirtualCam).
+// Open the first available device (will be an instance of a virtual camera simulator in this case).
 GcDevice camera = system.OpenDevice(cameraList[0]);
 
-// Fetch a single image and save it to file (via Emgu.CV). Image should show a grayscale image (320x240, 8-bit) of a vertical ramp moving testpattern.
+// Fetch a single image and save it to file (via Emgu.CV.Mat format). Image should be a grayscale image of a vertical ramp moving testpattern (320x240 pixels, 8-bit).
 camera.FetchImage(out GcBuffer image);
 image.ToMat().Save("image1.tiff");
 
-// Update some camera parameters.
+// Update some camera parameters (for a complete list of available parameters see camera.Parameters.ToList()).
 camera.Parameters.SetParameterValue("Height", "480");
 camera.Parameters.SetParameterValue("Width", "640");
 camera.Parameters.SetParameterValue("PixelFormat", "RGB8");
@@ -31,12 +31,14 @@ GcDataStream dataStream = camera.OpenDataStream();
 // Start streaming images from the camera.
 dataStream.Start();
 
-// Wait for the acquisition to start to allow image retrieval. Save it. Image should show a RGB image (640x480, 8-bit) of a frame counter testpattern (frame count should be 1).
+// Suspend current thread to allow at least one image to be queued up. 
 Thread.Sleep(100);
-dataStream.RetrieveImage(out image);
+
+// Retrieve image from the front of the queue and save it to file. Image should be an RGB image of a frame counter testpattern with counter set to 1 (640x480, 8-bit).
+dataStream.RetrieveImage(out image); // alternatively, images can be retrieved by subscribing to the dataStream.BufferTransferred event. 
 image.ToMat().Save("image2.tiff");
 
-// Close camera (automatically closes datastream).
+// Close camera (note: this will also stop all streaming).
 camera.Close();
 
 // Close library.
